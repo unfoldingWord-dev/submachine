648b61ec7eb7d256de5563f9a14e942855d7e26d
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2024-07-04 16:34:43 +0200

    Several changes - Better handling of package installation allows for translation through intermediate language - Subtitle file for original language will always be generated, and will be used when no translation is available.

--- Readme.md
+++ Readme.md
@@ -4,11 +4,11 @@ This code is an attempt to create a fully automatic subtitling system for vidoes
 The basic code is taken from [Digital Ocean](https://www.digitalocean.com/community/tutorials/how-to-generate-and-add-subtitles-to-videos-using-python-openai-whisper-and-ffmpeg)
 and molded into a class with my own nips and tucks. The translation was not part of the original tutorial. 
 
-Audio transcription is done **locally** through [OpenAI Whisper](https://openai.com/research/whisper) model.
+Audio transcription is done **locally** through the [OpenAI Whisper](https://openai.com/research/whisper) model.
 Text translation is done **locally** through [ArgosTranslate](https://pypi.org/project/argostranslate/).
 
 ## Usage
-I have only tested this with mp4 videos, so YYMV. Here are the steps to do it yourself. 
+I have only tested this with mp4 videos, so YYMV. Here are the steps. 
 
 1) Clone this repo
 ```bash
@@ -32,4 +32,106 @@ This will take a while, as the Whisper model is quite big.
 ```bash
 python3 main.py
 ```
-For every language that you want a subtitle for, an additional package will be downloaded during runtime. This can take some time. 
+For every language that you want a subtitle for, an additional package will be downloaded during runtime. This can take some time.
+
+## On language packs
+> Argos Translate [...] manages automatically pivoting through 
+> intermediate languages to translate between languages that don't have 
+> a direct translation between them installed. For example, if you have 
+> a **es → en** and **en → fr** translation installed you are able to translate 
+> from **es → fr** as if you had that translation installed. This allows 
+> for translating between a wide variety of languages at the cost of some 
+> loss of translation quality.
+
+*https://pypi.org/project/argostranslate/*
+
+The following language packs (and thus direct translations) are currently available:
+
+- Albanian -> English
+- Arabic -> English
+- Azerbaijani -> English
+- Bengali -> English
+- Bulgarian -> English
+- Catalan -> English
+- Chinese (traditional) -> English
+- Chinese -> English
+- Czech -> English
+- Danish -> English
+- Dutch -> English
+- English -> Albanian
+- English -> Arabic
+- English -> Azerbaijani
+- English -> Bengali
+- English -> Bulgarian
+- English -> Catalan
+- English -> Chinese
+- English -> Chinese (traditional)
+- English -> Czech
+- English -> Danish
+- English -> Dutch
+- English -> Esperanto
+- English -> Estonian
+- English -> Finnish
+- English -> French
+- English -> German
+- English -> Greek
+- English -> Hebrew
+- English -> Hindi
+- English -> Hungarian
+- English -> Indonesian
+- English -> Irish
+- English -> Italian
+- English -> Japanese
+- English -> Korean
+- English -> Latvian
+- English -> Lithuanian
+- English -> Malay
+- English -> Norwegian
+- English -> Persian
+- English -> Polish
+- English -> Portuguese
+- English -> Romanian
+- English -> Russian
+- English -> Slovak
+- English -> Slovenian
+- English -> Spanish
+- English -> Swedish
+- English -> Tagalog
+- English -> Thai
+- English -> Turkish
+- English -> Ukranian
+- English -> Urdu
+- Esperanto -> English
+- Estonian -> English
+- Finnish -> English
+- French -> English
+- German -> English
+- Greek -> English
+- Hebrew -> English
+- Hindi -> English
+- Hungarian -> English
+- Indonesian -> English
+- Irish -> English
+- Italian -> English
+- Japanese -> English
+- Korean -> English
+- Latvian -> English
+- Lithuanian -> English
+- Malay -> English
+- Norwegian -> English
+- Persian -> English
+- Polish -> English
+- Portuguese -> English
+- Portuguese -> Spanish
+- Romanian -> English
+- Russian -> English
+- Slovak -> English
+- Slovenian -> English
+- Spanish -> English
+- Spanish -> Portuguese
+- Swedish -> English
+- Tagalog -> English
+- Thai -> English
+- Turkish -> English
+- Ukranian -> English
+- Urdu -> English
--- main.py
+++ main.py
@@ -40,37 +40,79 @@ class SubMachine:
                   (segment.start, segment.end, segment.text))
         return language, segments
 
-    def __translate_argos(self, segments, from_lc, to_lc):
-
+    def __install_translation_packages(self, from_lc, to_lc):
         # Download and install Argos Translate package
         argostranslate.package.update_package_index()
         available_packages = argostranslate.package.get_available_packages()
-        package_to_install = next(
-            filter(
-                lambda x: x.from_code == from_lc and x.to_code == to_lc, available_packages
+
+        try:
+            package_to_install = next(
+                filter(
+                    lambda x: x.from_code == from_lc and x.to_code == to_lc, available_packages
+                )
             )
-        )
-        argostranslate.package.install_from_path(package_to_install.download())
+            argostranslate.package.install_from_path(package_to_install.download())
 
-        # We keep timestamps out of the translation, as they are being localized as well,
-        # which would make the resulting .srt file 'corrupt'.
-        text = self.__create_translatable_text(segments)
+        except StopIteration:
+            # Can't find direct translation route. Will try to use multiple packages
+            # Warning: this only works because I assume that you somehow already installed the intermediate packages
+            print(f'No direct translation from \'{from_lc}\' to \'{to_lc}\' available. Will try to use intermediate'
+                  f'translation (en). This will result in a slightly degraded translation.')
 
-        # print(text)
+            intermediate_lc = 'en'
 
-        translated_text = argostranslate.translate.translate(text, from_lc, to_lc)
+            try:
+                package_to_install = next(
+                    filter(
+                        lambda x: x.from_code == from_lc and x.to_code == intermediate_lc, available_packages
+                    )
+                )
+                argostranslate.package.install_from_path(package_to_install.download())
 
-        # print(translated_text)
+                try:
+                    package_to_install = next(
+                        filter(
+                            lambda x: x.from_code == intermediate_lc and x.to_code == to_lc, available_packages
+                        )
+                    )
+                    argostranslate.package.install_from_path(package_to_install.download())
+                except StopIteration:
+                    print(f'No intermediate translation from \'{intermediate_lc}\' to \'{to_lc}\' available.')
+                    return False
+            except StopIteration:
+                print(f'No intermediate translation from \'{from_lc}\' to \'{intermediate_lc}\' available.')
+                return False
 
-        for index, segment in enumerate(segments):
-            segment_start = self.__format_time(segment.start)
-            segment_end = self.__format_time(segment.end)
+        # All went well. We could install a direct translation package,
+        # or we were able to install intermediate packages
+        return True
 
-            translated_text = translated_text.replace(f'[{str(index + 1)}]', f"{segment_start} --> {segment_end} ")
+    def __translate_argos(self, segments, from_lc, to_lc):
 
-        # print(translated_text)
+        if self.__install_translation_packages(from_lc, to_lc):
 
-        return translated_text
+            # We keep timestamps out of the translation, as they are being localized as well,
+            # which would make the resulting .srt file 'corrupt'.
+            text = self.__create_translatable_text(segments)
+
+            # print(text)
+
+            translated_text = argostranslate.translate.translate(text, from_lc, to_lc)
+
+            # print(translated_text)
+
+            for index, segment in enumerate(segments):
+                segment_start = self.__format_time(segment.start)
+                segment_end = self.__format_time(segment.end)
+
+                translated_text = translated_text.replace(f'[{str(index + 1)}]', f"{segment_start} --> {segment_end} ")
+
+            # print(translated_text)
+
+            return translated_text
+
+        else:
+            return None
 
     def __format_time(self, seconds):
         hours = math.floor(seconds / 3600)
@@ -87,8 +129,6 @@ class SubMachine:
         text = ""
 
         for index, segment in enumerate(segments):
-            # segment_start = self.__format_time(segment.start)
-            # segment_end = self.__format_time(segment.end)
             text += f"{str(index + 1)} \n"
             text += f"[{str(index + 1)}] \n"
             text += f"{segment.text} \n"
@@ -122,7 +162,7 @@ class SubMachine:
 
         video_input_stream = ffmpeg.input(self.__input_video)
         subtitle_input_stream = ffmpeg.input(subtitle_file)
-        output_video = f"{self.__output_dir}/{self.__input_video_name}-output-{subtitle_language}.mp4"
+        output_video = f"{self.__output_dir}/{self.__input_video_name}-with-subs-{subtitle_language}.mp4"
         subtitle_track_title = subtitle_file.replace(".srt", "")
 
         if soft_subtitle:
@@ -150,19 +190,20 @@ class SubMachine:
         # Transcribe audio
         language, segments = self.__transcribe(audio)
 
-        # Translate transcription
+        # Always create subtitle with original language
+        subtitles = self.__parse_segments_to_srt(segments)
+        sub_lc = language
+        subtitle_file = self.__generate_subtitle_file(sub_lc, subtitles)
+
+        # If sub has a language code, translate the subs to that lc
         if sub:
-            target_lang = sub
-            # First translate subtitles
-            subtitles = self.__translate_argos(segments, from_lc=language, to_lc=target_lang)
-
-        else:
-            target_lang = language
-            # Just generate subtitle file from transcription
-            subtitles = self.__parse_segments_to_srt(segments)
-
-        # Create the subtitle file
-        subtitle_file = self.__generate_subtitle_file(target_lang, subtitles)
+            if sub != language:
+                sub_lc = sub
+                subtitles = self.__translate_argos(segments, from_lc=language, to_lc=sub_lc)
+                if subtitles:
+                    subtitle_file = self.__generate_subtitle_file(sub_lc, subtitles)
+                else:
+                    print(f'Could not translate subtitles. Using original language ({language})')
 
         # Glue transcription and video together
         soft_subtitle = True if os.getenv('BURNIN') == 'False' else False
@@ -170,7 +211,7 @@ class SubMachine:
         self.__add_subtitle_to_video(
             soft_subtitle=soft_subtitle,
             subtitle_file=subtitle_file,
-            subtitle_language=target_lang
+            subtitle_language=sub_lc
         )
 
 
